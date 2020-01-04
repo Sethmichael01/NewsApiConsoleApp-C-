a2984def4588c8a0ff4fb5da19dc2757bd8f0dc4
Longe Michael Seth <49845849+Sethmichael01@users.noreply.github.com>
2020-01-04 01:38:23 +0100

    Add files via upload

---
+++ NewsApi/NewsApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace NewsApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://newsapi.org/v2/top-headlines?" +
+          "country=us&" +
+          "apiKey=27e5ee2f6d704aaa8be077730cd1d85c";
+            var json = new WebClient().DownloadString(url);
+            Console.WriteLine(json);
+        }
+    }
+}
